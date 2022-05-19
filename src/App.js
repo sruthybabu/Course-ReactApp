83ae3ad4aaf2081700643a46e5a99c29fdb89671
sruthybabu <sruthybabu64@gmail.com>
2022-05-19 11:45:32 +0530

    Course view complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addcourse from './components/Addcourse';
+import Viewcourse from './components/Viewcourse';
 
 function App() {
   return (
    <div>
 
 <Addcourse/>
+<Viewcourse/>
 
    </div>
   );
